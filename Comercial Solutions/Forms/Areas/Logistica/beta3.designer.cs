11228c23d59b70562ada11ab71607db344624925
Enrique Magnani <kikemaxmq@hotmail.com>
2014-04-14 05:40:59 -0600

    Inicio
    Acceso a BD
    Autenticacion
    Control de memoria

---
+++ Comercial Solutions/Forms/Areas/Logistica/beta3.designer.cs
@@ -0,0 +1,48 @@
+﻿namespace Comercial_Solutions.Forms.Areas.Logistica
+{
+    partial class beta3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // beta3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "beta3";
+            this.Text = "beta3";
+            this.Load += new System.EventHandler(this.beta3_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
